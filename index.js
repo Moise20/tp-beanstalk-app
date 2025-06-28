535e3da52545aa73231ab9fea2f92e46489aee50
PANA <essognim-moise.pana@soprasteria.com>
2025-06-28 15:40:24 +0200

    feat: Updated welcome message to test pipeline trigger

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 8081; // <-- TRÈS IMPORTANT : Elastic Beanstalk définit le port via une variable d'environnement
 
 app.get('/', (req, res) => {
-  res.send('Bienvenue sur Elastic Beanstalk de Moiïse PANA ');
+  res.send('Hello from my automated CI/CD pipeline de Moïse PANA !');
 });
 
 app.listen(port, () => {
